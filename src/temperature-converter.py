3b8ff6fc415aac4af13e9e48e8d5ce62a2603965
rkspx <kosiriki@gmail.com>
2025-03-01 15:07:50 +0700

    add defensive code for temperature conversion

--- src/temperature-converter.py
+++ src/temperature-converter.py
@@ -1,17 +1,72 @@
+class InvalidUnitException(Exception):
+    def __init__(self):
+        super().__init__("Invalid conversion unit, the conversion unit should be either 'f' or 'c'")
+
+class InvalidConversionValue(Exception):
+    def __init__(self):
+        super().__init__("Invalid conversion value, the value should be either numeric string, int, or float")
+
 def convert_temperature(value, unit):
-    result = 0
-    resultUnit = "c"
-    if unit == "c":
-        resultUnit = "f"
-    if unit == "f":
-        result =  (value - 32) * 5/9
-    else:
-        result =  (value * 9/5) + 32
-    print(f"converted {value}{unit} is {result}{resultUnit}")
-    return result
+    unit = unit.lower()
+    if unit != "f" and unit !="c":
+        raise InvalidUnitException()
     
+    if not isinstance(value, (int, float, str)):
+        raise InvalidConversionValue()
+    if isinstance(value, str) and not value.isdigit():
+        raise InvalidConversionValue()
+    
+    conversion = {
+        "f":lambda value: (value - 32) * 5/9,
+        "c": lambda value: (value * 9/5) + 32
+    }
+
+    if unit in conversion:
+        return conversion[unit](float(value))
+    else :
+        raise InvalidUnitException()
 
 assert convert_temperature(0, "c") == 32
 assert convert_temperature(100, "c") == 212
 assert convert_temperature(32,"f") == 0
 assert convert_temperature(212,"f") == 100
+
+assert convert_temperature('0', "c") == 32
+assert convert_temperature('100', "c") == 212
+assert convert_temperature('32',"f") == 0
+assert convert_temperature('212',"f") == 100
+
+assert convert_temperature(0, "C") == 32
+assert convert_temperature(100, "C") == 212
+assert convert_temperature(32,"F") == 0
+assert convert_temperature(212,"F") == 100
+
+assert convert_temperature('0', "C") == 32
+assert convert_temperature('100', "C") == 212
+assert convert_temperature('32',"F") == 0
+assert convert_temperature('212',"F") == 100
+
+try:
+    convert_temperature(0, "x") == 32
+except InvalidUnitException:
+    print("invalid value properly captured")
+
+try:
+    convert_temperature({}, "c") == 32
+except InvalidConversionValue:
+    print("invalid value properly captured")
+
+try:
+    convert_temperature("abcd", "c") == 32
+except InvalidConversionValue:
+    print("invalid value properly captured")
+
+try:
+    convert_temperature((), "c") == 32
+except InvalidConversionValue:
+    print("invalid value properly captured")
+
+try:
+    convert_temperature([], "c") == 32
+except InvalidConversionValue:
+    print("invalid value properly captured")
